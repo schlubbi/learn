fcec492deeeda1c70c5e8dbd34e6aaf164865084
Stefan Schmidt <mail@schlubbi.io>
2016-01-10 19:56:13 +0100

    add ex10-15

---
+++ learn_c_the_hard_way/ex14.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int can_print_it(char ch)
+{
+	return isalpha(ch) || isblank(ch);
+}
+
+void print_letters(char arg[])
+{
+	for(int i = 0; arg[i]; i++) {
+		char ch = arg[i];
+		if (can_print_it(ch)) {
+			printf("'%c' == %d ", ch, ch);
+		}
+	}
+	printf("\n");
+}
+
+void print_arguments(int argc, char *argv[])
+{
+	for(int i = 0; i < argc; i++) {
+		print_letters(argv[i]);
+	}
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	print_arguments(argc + 1, argv);
+	return 0;
+}
