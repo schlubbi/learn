2d07b1a31082eef3ce6bfa4e932c15ecca5f030a
Stefan Schmidt <mail@schlubbi.io>
2016-01-24 17:33:43 +0100

    add test cases

--- c/learn_c_the_hard_way/krisso.c
+++ c/learn_c_the_hard_way/krisso.c
@@ -29,10 +29,30 @@ int str_cmp(char *a, char *b)
   return *a > *b ? 1 : -1 ;
 }
 
+void test_equals()
+{
+  assert(equals("abc","abc") == 1);
+  assert(equals("a", "bcd") == 0);
+  assert(equals("", "") == 1);
+  assert(equals(" ", " ") == 1);
+  assert(equals(" ", "abc ") == 0);
+  assert(equals(" ", " abc ") == 0);
+}
+
+void test_str_cmp()
+{
+  assert(str_cmp("","") == 0);
+  assert(str_cmp("Hello ","b") == -1);
+  assert(str_cmp("Hello","Abc") == 1);
+
+}
+
 int main(int argc, char *argv[])
 {
-  assert(str_cmp("Hello World","Hello%20World") == 0);
-  assert(str_cmp("Hello ","Hello%20") == 0);
-  assert(str_cmp("  Hello","%20%20Hello") == 0);
+  test_str_cmp();
+  test_equals();
+  //assert(str_cmp("Hello World","Hello%20World") == 0);
+  //assert(str_cmp("Hello ","Hello%20") == 0);
+  //assert(str_cmp("  Hello","%20%20Hello") == 0);
   return 0;
 }
