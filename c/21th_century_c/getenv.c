d8f704d48c44cc131723d19d93ac664c5e60cf8a
Stefan Schmidt <mail@schlubbi.io>
2016-03-13 19:45:21 +0100

    start 21th century C

---
+++ c/21th_century_c/getenv.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+  char *repstext = getenv("reps");
+  int reps = repstext ? atoi(repstext) : 10;
+
+  char *msg = getenv("msg");
+  if (!msg) msg = "Hello.";
+  for (int i = 0; i < reps; i++)
+    printf("%s\n", msg);
+}
