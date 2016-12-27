f62a03811ba4a10867e1a8d53588d8b34a09f3b2
Stefan Joest <mail@schlubbi.io>
2016-12-27 21:55:00 +0100

    first take on project4 Fill

--- coursera/nand2tetris/projects/04/fill/Fill.asm
+++ coursera/nand2tetris/projects/04/fill/Fill.asm
@@ -1,11 +1,59 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+@SCREEN
+D=A
+@arr
+M=D // save beginning of SCREEN memory address space to arr
+@KBD
+D=A
+@SCREEN
+D=D-A
+@SCREENSZIE
+M=D // set SCREENSZIE to the last bit of the SCREEN address space
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+(ENDLESSLOOP)
+@i
+M=0 // init i = 0
+(LOOP)
+    @i
+    D=M
+    @SCREENSZIE
+    D=D-M
+    @END
+    D;JEQ // compare i and SCREENSIZE
 
-// Put your code here.
+    @arr
+    D=M
+    @i
+    A=D+M
+    D=A
+    @R1
+    M=D
+
+    @KBD
+    D=M
+
+    @BLACK
+    D;JNE
+
+    @R1
+    A=M
+    M=0 // SET RAM[SCREEN+i] to 0 (white)
+    @CONTINUE
+    0;JMP
+
+    (BLACK)
+    @R1
+    A=M
+    M=-1 // SET RAM[SCREEN+i] to -1 (black)
+    @CONTINUE
+    0;JMP
+    
+    
+    (CONTINUE)
+    @i
+    M=M+1 // increment i
+    
+    @LOOP
+    0;JMP
+(END)
+    @ENDLESSLOOP
+    0;JMP
